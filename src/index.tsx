d877a9b3360b64043adb45999a9e905918ebf69f
Tory Hebert <tory37@gmail.com>
2019-12-01 15:49:16 -0600

    finished video 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import AWSAppSyncClient from "aws-appsync";
+import AppSyncConfig from "./aws-exports";
+import { ApolloProvider } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { AuthOptions, AUTH_TYPE } from "aws-appsync-auth-link";
+
+const client = new AWSAppSyncClient({
+  url: AppSyncConfig.aws_appsync_graphqlEndpoint,
+  region: AppSyncConfig.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.API_KEY,
+    apiKey: AppSyncConfig.aws_appsync_apiKey
+  }
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
