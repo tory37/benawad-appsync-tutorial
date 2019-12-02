9d9707e574d625ef1dbbff5e20d81a56aceee53c
Tory Hebert <tory37@gmail.com>
2019-12-02 13:55:31 -0600

    finished video 5

--- src/components/Auctions.tsx
+++ src/components/Auctions.tsx
@@ -3,8 +3,11 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 import AuctionCard from "components/AuctionCard";
+import OnMount from "components/OnMount";
 import { listAuctions } from "graphql/queries";
 import { ListAuctionsQuery, ListAuctionsQueryVariables } from "API";
+import { buildSubscription } from "aws-appsync";
+import { onCreateAuction } from "graphql/subscriptions";
 
 const Auctions = () => {
   return (
@@ -12,7 +15,7 @@ const Auctions = () => {
       query={gql(listAuctions)}
       variables={{ limit: 100 }}
     >
-      {({ data, loading }) => {
+      {({ data, loading, subscribeToMore }) => {
         if (
           loading ||
           !data ||
@@ -26,10 +29,17 @@ const Auctions = () => {
           <div
             style={{
               display: "grid",
-              gridTemplateColumns: "repeat(4, 1fr)",
+              gridTemplateColumns: "repeat(3, 1fr)",
               gridGap: 10
             }}
           >
+            <OnMount
+              onEffect={() => {
+                return subscribeToMore(
+                  buildSubscription(gql(onCreateAuction), gql(listAuctions))
+                );
+              }}
+            />
             {data.listAuctions.items.map(x => (
               <AuctionCard key={x!.id} name={x!.name} price={x!.price} />
             ))}
